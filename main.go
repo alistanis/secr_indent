f422adb5654a6c72753ec7d60139aec5be654bc9
Chris Cooper <christopher_cooper@harvard.edu>
2017-02-27 10:41:12 -0500

    add first version

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+var (
+	files string
+)
+
+func init() {
+	flag.StringVar(&files, "f", "", "Comma separated list of files to indent (will print them to stdout)")
+	flag.Parse()
+}
+
+func main() {
+	if files == "" {
+		fmt.Fprintln(os.Stderr, "Must provide -f")
+		flag.Usage()
+		os.Exit(-1)
+	}
+
+	paths := strings.Split(files, ",")
+
+	for _, p := range paths {
+		data, err := ioutil.ReadFile(p)
+		if err != nil {
+			exitErr(err)
+		}
+		buf := bytes.NewBuffer([]byte("    "))
+		for _, b := range data {
+			buf.WriteByte(b)
+			if b == '\n' {
+				buf.WriteString("    ")
+			}
+		}
+		fmt.Println(buf.String())
+	}
+
+}
+
+// Insert inserts insertData into data at the given start index
+func insert(data, insertData []byte, start int) []byte {
+	return append(data[:start], append(insertData, data[start:]...)...)
+}
+
+func exitErr(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(-1)
+}
